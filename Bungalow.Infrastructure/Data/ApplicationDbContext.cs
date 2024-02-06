19c9a54e6a97aa723e3d9cf2e4bf7642b9b65537
FAArik <62200015+FAArik@users.noreply.github.com>
2024-02-07 00:22:49 +0300

    seed data

--- Bungalow.Infrastructure/Data/ApplicationDbContext.cs
+++ Bungalow.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,12 +1,49 @@
 ﻿using BungalowApi.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace BungalowApi.Infrastructure.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+
     }
+
     public DbSet<Bungalow> Bungalows { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Bungalow>().HasData(
+            new Bungalow
+            {
+                Id = 1,
+                Name = "Royal Bungalow",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x480",
+                Occupancy = 4,
+                Price = 200,
+                Sqft = 550,
+            },
+            new Bungalow
+            {
+                Id = -2,
+                Name = "Premium Pool Bungalow",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x401",
+                Occupancy = 4,
+                Price = 300,
+                Sqft = 550,
+            }, new Bungalow
+            {
+                Id = 3,
+                Name = "Luxury Pool Bungalow",
+                Description = "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+                ImageUrl = "https://placehold.co/600x402",
+                Occupancy = 4,
+                Price = 400,
+                Sqft = 750
+            });
+    }
 }
---
+++ Bungalow.Web/Controllers/BungalowController.cs
@@ -0,0 +1,20 @@
+﻿using BungalowApi.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BungalowApi.Web.Controllers;
+
+public class BungalowController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public BungalowController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var bungalows = _context.Bungalows.ToList();
+        return View();
+    }
+}
